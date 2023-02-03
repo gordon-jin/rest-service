ec42492dc0f41006ba9b51314a964e305b55b1d0
Toshiaki Maki <tmaki@vmware.com>
2023-02-04 01:40:13 +0900

    Add log

--- src/main/java/com/example/restservice/Greeting.java
+++ src/main/java/com/example/restservice/Greeting.java
@@ -18,4 +18,12 @@ public class Greeting {
 	public String getContent() {
 		return content;
 	}
+
+	@Override
+	public String toString() {
+		return "Greeting{" +
+				"id=" + id +
+				", content='" + content + '\'' +
+				'}';
+	}
 }
--- src/main/java/com/example/restservice/GreetingController.java
+++ src/main/java/com/example/restservice/GreetingController.java
@@ -2,6 +2,9 @@ package com.example.restservice;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,8 +18,12 @@ public class GreetingController {
 
 	private final AtomicLong counter = new AtomicLong();
 
+	private final Logger log = LoggerFactory.getLogger(GreetingController.class);
+
 	@GetMapping("/greeting")
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+		final Greeting greeting = new Greeting(counter.incrementAndGet(), String.format(template, name));
+		log.info("{}", greeting);
+		return greeting;
 	}
 }
